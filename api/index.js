246653673fe1e35b78afb9ebd89f15aebcb945b5
lawrence <lawrenceanthony70@gmail.com>
2023-03-08 20:46:28 +0100

    testing new updates

--- api/index.js
+++ api/index.js
@@ -5,20 +5,34 @@ var __importDefault =
     return mod && mod.__esModule ? mod : { default: mod };
   };
 Object.defineProperty(exports, "__esModule", { value: true });
-var apollo_server_1 = require("apollo-server");
-var schema_1 = __importDefault(require("./schema"));
+// var apollo_server_1 = require("apollo-server"); old code
+
+var apollo_server_1 = require("apollo-server-micro");
+import { send } from "micro";
+import Cors from "micro-cors";
+var schema_1 = __importDefault(require("../schema"));
 var resolvers_1 = __importDefault(require("../resolvers"));
 var server = new apollo_server_1.ApolloServer({
   typeDefs: schema_1.default,
   resolvers: resolvers_1.default,
 });
 
-let serverPort = process.env.PORT || 4000;
-let serverHost = "0.0.0.0";
+const cors = Cors();
 
-server.listen(serverPort, serverHost, function () {
-  console.log(`\uD83D\uDE80  Server ready at ${serverPort}`);
+export default server.start().then(() => {
+  const handler = server.createHandler({ path: "/api/index" });
+
+  return cors((req, res) => {
+    return req.method === "OPTIONS" ? send(res, 200, "ok") : handler(req, res);
+  });
 });
+
+// let serverPort = process.env.PORT || 4000;
+// let serverHost = "0.0.0.0";
+
+// server.listen(serverPort, serverHost, function () {
+//   console.log(`\uD83D\uDE80  Server ready at ${serverPort}`);
+// });
 
 // server.listen().then(function (_a) {
 //   _a.url = process.env.PORT;
